b722365db610ec002bd77f80c59dae0673802927
RyanHx <hollick5@hotmail.com>
2022-04-23 14:04:25 +0100

    Fix countdown over 1 day

--- src/components/PlantBox.js
+++ src/components/PlantBox.js
@@ -2,15 +2,16 @@ import React from 'react'
 import { Container, ListGroup, Row, Col, Button } from 'react-bootstrap';
 import { FiX, FiPlay, FiSkipForward, FiRefreshCw } from "react-icons/fi";
 import Countdown from 'react-countdown';
+import { zeroPad } from 'react-countdown';
 
 const PlantBox = ({ profile, plantData, onDelete, plantContinue, restartPlant }) => {
-    const renderer = ({ hours, minutes, seconds, completed }) => {
+    const renderer = ({formatted: {hours, minutes, seconds}, completed}) => {
         if (completed) {
             // Render a completed state
             return <p className='text-success'>Completed</p>;
         } else {
             // Render a countdown
-            return <p>Time left: {hours < 10 ? '0' + hours : hours}:{minutes < 10 ? '0' + minutes : minutes}:{seconds < 10 ? '0' + seconds : seconds}</p>;
+            return <p>Time left: {zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}</p>;
         }
     };
 
@@ -26,7 +27,7 @@ const PlantBox = ({ profile, plantData, onDelete, plantContinue, restartPlant })
                                     <Col className='col-6'>
                                         <h3>{basePlant.name}</h3>
                                         <p>Stage: {plantData.stageNames[plant.currentStage]}</p>
-                                        <Countdown key={plant.finishDate} date={plant.finishDate} renderer={renderer} />
+                                        <Countdown key={plant.finishDate} daysInHours={true} date={plant.finishDate} renderer={renderer} />
                                     </Col>
                                     <Col className='align-self-center'>
                                         <Button variant='success' onClick={() => plantContinue(profile.id, plant.id, false)}><FiPlay /></Button>{' '}
