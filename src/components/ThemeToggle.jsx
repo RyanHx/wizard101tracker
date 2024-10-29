b3d9baf1bda06bcae0a7e81aa3ae914ebb8cb347
RyanHx <19942516+RyanHx@users.noreply.github.com>
2024-10-29 15:21:31 +0000

    Added support for browser level theme preference.

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,25 +1,37 @@
 import Button from 'react-bootstrap/Button';
 import BootStrapIcon from './BootStrapIcon';
-import { useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 export default function ThemeToggle() {
-    const getIsDarkThemePreferred = () => {
-        const isDarkThemeStored = localStorage.getItem('isDarkTheme')
-        if (isDarkThemeStored !== null) {
-            return isDarkThemeStored === 'true';
+    const preferredTheme = () => {
+        const theme = localStorage.getItem('theme')
+        if (theme !== null) {
+            return theme;
         }
-        return window.matchMedia('(prefers-color-scheme: dark)').matches;
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
     }
-    const [isDarkMode, setIsDarkMode] = useState(getIsDarkThemePreferred());
-    const setTheme = isDark => {
-        setIsDarkMode(isDark);
-        localStorage.setItem('isDarkTheme', isDark);
-        document.documentElement.setAttribute('data-bs-theme', isDark ? 'dark' : 'light');
-    }
-    
+    const [theme, setTheme] = useState(preferredTheme());
+    const toggleTheme = useCallback((save) => {
+        const newTheme = theme === 'dark' ? 'light' : 'dark';
+        setTheme(newTheme);
+        if (save) {
+            localStorage.setItem('theme', newTheme);
+        }
+        document.documentElement.setAttribute('data-bs-theme', newTheme);
+    }, [theme]);
+
+    useEffect(() => {
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+            console.log('browser preferred theme changed');
+            if (theme !== preferredTheme()) {
+                toggleTheme(false);
+            }
+        });
+    }, [theme, toggleTheme]);
+
     return (
-        <Button id="bd-theme" onClick={() => setTheme(!isDarkMode)} variant="outline-primary" className='align-items-center' aria-label={`Toggle theme (${isDarkMode ? 'dark' : 'light'})`}>
-            <BootStrapIcon iconId={isDarkMode ? 'moon-stars-fill' : 'sun-fill'} className='opacity-75' />
+        <Button id="bd-theme" onClick={() => toggleTheme(true)} variant="outline-primary" className='align-items-center' aria-label='Toggle theme (theme)'>
+            <BootStrapIcon iconId={theme === 'dark' ? 'moon-stars-fill' : 'sun-fill'} className='opacity-75' />
             <span className="d-lg-none ms-2" id="bd-theme-text">Toggle theme</span>
         </Button>
     );
